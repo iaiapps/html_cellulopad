9e23372d17d36ad1a50e7104c324be062cfff432
iaiapps <ikromudin.ai@gmail.com>
2022-03-16 17:20:31 +0700

    first commit

---
+++ quizz/pilgan/js/questions.js
@@ -0,0 +1,125 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Urutan organisasi kehidupan dari terkecil ke terbesar yang benar adalah . . .",
+    answer: "C. sel – jaringan – organ – sistem organ – organisme",
+    options: [
+      "A. sel – jaringan -sistem organ – organ – organisme",
+      "B. sel – organ – jaringan – sistem organ – organisme",
+      "C. sel – jaringan – organ – sistem organ – organisme",
+      "D. sel – sistem organ – jaringan – organ -organisme",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Bagian terkecil dari makhluk hidup adalah . . .",
+    answer: "A. sel",
+    options: ["A. sel", "B. jaringan", "C. organ", "D. sistem organ"],
+  },
+  {
+    numb: 3,
+    question:
+      "Sel tumbuhan dan sel hewan memiliki perbedaan walaupun secara umum organelnya sama. Perbedaannya antara lain . . .",
+    answer: "A. sel tumbuhan memiliki kloroplas, sel hewan tidak",
+    options: [
+      "A. sel tumbuhan memiliki kloroplas, sel hewan tidak",
+      "B. sel tumbuhan tidak memiliki kloroplas, sel hewan punya",
+      "C. sel hewan mengandung selulosa, sel tumbuhan tidak",
+      "D. sel hewan tidak memiliki membran inti, sel tumbuhanpunya",
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "Berikut ini bagian dari sel yang dimiliki oleh hewan maupun tumbuhan adalah . . .",
+    answer: "A. membran sel",
+    options: [
+      "A. membran sel",
+      "B. kloroplas",
+      "C. plastida",
+      "D. dinding sel",
+    ],
+  },
+  {
+    numb: 5,
+    question:
+      "Tubuh mahluk hidup tersusun oleh berbagai jenis jaringan sel. Jaringan terbentuk apabila . . .",
+    answer: "B. beberapa sel mempunyai bentuk dan fungsi yang sama",
+    options: [
+      "A. beberapa sel mempunyai bentuk dan fungsi yang tidak sama",
+      "B. beberapa sel mempunyai bentuk dan fungsi yang sama",
+      "C. organisasi kehidupan yang paling kecil",
+      "D. mahluk hidup tersebut bersel banyak",
+    ],
+  },
+  {
+    numb: 6,
+    question:
+      "Mengapa tumbuhan berwarna hijau sedangkan kulit harimau berwarna coklat? Sel tumbuhan dan sel hewan memiliki perbedaan walaupun secara umum organelnya sama. Perbedaannya antara lain . . .",
+    answer: "A. sel tumbuhan memiliki kloroplas, sel hewan tidak",
+    options: [
+      "A. sel tumbuhan memiliki kloroplas, sel hewan tidak",
+      "B. sel tumbuhan tidak memiliki kloroplas, sel hewan punya",
+      "C. sel hewan mengandung selulosa, sel tumbuhan tidak",
+      "D. sel hewan tidak memiliki membrane inti, sel tumbuhan punya",
+    ],
+  },
+  {
+    numb: 7,
+    question:
+      "Tubuh mahluk hidup tersusun oleh berbagai jenis jaringan sel. Jaringan terbentuk apabila . . .",
+    answer: "B. beberapa sel mempunyai bentuk dan fungsi yang sama",
+    options: [
+      "A. beberapa sel mempunyai bentuk dan fungsi yang tidak sama",
+      "B. beberapa sel mempunyai bentuk dan fungsi yang sama",
+      "C. organisasi kehidupan yang paling kecil",
+      "D. mahluk hidup tersebut bersel banyak",
+    ],
+  },
+  {
+    numb: 8,
+    question: "Organel yang fungsinya untuk pernapasan sel adalah . . .",
+    answer: "B. mitokondria",
+    options: ["A. lisosom", "B. mitokondria", "C. badan golgi", "D. kloroplas"],
+  },
+  {
+    numb: 9,
+    question:
+      "Struktur dasar sel dari yang terluar baik pada sel tumbuhan maupun sel hewan yaitu . . .",
+    answer: "B. membran plasma, sitoplasma, dan inti",
+    options: [
+      "A. membran inti, sitoplasma, dan inti",
+      "B. membran plasma, sitoplasma, dan inti",
+      "C. sitoplasma, membran inti, dan organel",
+      "D. inti, organel, sitoplasma",
+    ],
+  },
+  {
+    numb: 10,
+    question:
+      "Manakah yang bisa Ananda cubit, batang tanaman atau kulit kucing? Sel tumbuhan lebih kuat dan lebih kaku dibanding sel hewan karena sel tumbuhan memiliki . . .",
+    answer: "A. dinding sel",
+    options: [
+      "A. dinding sel",
+      "B. membran sel",
+      "C. seludang protein",
+      "D. kapsid",
+    ],
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
